8c8cc6387607d49fcb219a3f6e4ada3dc53fec51
赫赫 <wenyu_0818@qq.com>
2025-06-14 13:58:03 +0800

    Update listWidgetItems.py

--- custom/listWidgetItems.py
+++ custom/listWidgetItems.py
@@ -120,37 +120,6 @@ class EdgeItem(MyItem):
         return img
 
 
-class ContourItem(MyItem):
-    def __init__(self, parent=None):
-        super(ContourItem, self).__init__('轮廓检测', parent=parent)
-        self._mode = TREE_CONTOUR_MODE
-        self._method = SIMPLE_CONTOUR_METHOD
-        self._bbox = NORMAL_CONTOUR
-
-    def __call__(self, img):
-        mode = CONTOUR_MODE[self._mode]
-        method = CONTOUR_METHOD[self._method]
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cnts, _ = cv2.findContours(img, mode, method)
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        if self._bbox == RECT_CONTOUR:
-            bboxs = [cv2.boundingRect(cnt) for cnt in cnts]
-            print(bboxs)
-            for x, y, w, h in bboxs:
-                img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
-        elif self._bbox == MINRECT_CONTOUR:
-            bboxs = [np.int0(cv2.boxPoints(cv2.minAreaRect(cnt))) for cnt in cnts]
-            img = cv2.drawContours(img, bboxs, -1, (255, 0, 0), thickness=2)
-        elif self._bbox == MINCIRCLE_CONTOUR:
-            circles = [cv2.minEnclosingCircle(cnt) for cnt in cnts]
-            print(circles)
-            for (x, y), r in circles:
-                img = cv2.circle(img, (int(x), int(y)), int(r), (255, 0, 0), thickness=2)
-        elif self._bbox == NORMAL_CONTOUR:
-            img = cv2.drawContours(img, cnts, -1, (255, 0, 0), thickness=2)
-
-        return img
-
 
 class EqualizeItem(MyItem):
     def __init__(self, parent=None):
